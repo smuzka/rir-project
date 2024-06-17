f95c5a6838d8ce05ec1db78a2698e54b7adf59d6
Jakub Smuga <jakub.smuga@weastronauts.com>
2024-06-17 14:45:30 +0200

    Refactor code os it uses PGAS UPC++ technology

--- main.cpp
+++ main.cpp
@@ -1,156 +1,184 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "mpi.h"
-#include <time.h>
-#include <math.h>
+#include <upcxx/upcxx.hpp>
+#include <iostream>
+#include <fstream>
+#include <vector>
 
-// Funkcja do przesunięcia macierzy o k kroków w poziomie
-void shift_matrix_horizontal(int* cell, int k, MPI_Comm comm) {
-    int source, dest;
-    MPI_Status status;
+using namespace std;
+using namespace upcxx;
 
-    MPI_Cart_shift(comm, 1, -k, &source, &dest);
-    MPI_Sendrecv_replace(cell, 1, MPI_INT, dest, 0, source, 0, comm, &status);
+// Funkcja do przesunięcia pozycji o k kroków w poziomie na prawo
+void shift_matrix_horizontal(int &local_cell, global_ptr<int> matrix, int k, int size, int &position)
+{
+    position = (position + k) % size + (position / size) * size; // położenie na osi x + przesunięcie na osi y
+    local_cell = rget(matrix + position).wait();
 }
 
-// Funkcja do przesunięcia macierzy o k kroków w pionie
-void shift_matrix_vertical(int* cell, int k, MPI_Comm comm) {
-    int source, dest;
-    MPI_Status status;
-
-    MPI_Cart_shift(comm, 0, -k, &source, &dest);
-    MPI_Sendrecv_replace(cell, 1, MPI_INT, dest, 0, source, 0, comm, &status);
+// Funkcja do przesunięcia pozycji o k kroków w pionie w dół
+void shift_matrix_vertical(int &local_cell, global_ptr<int> matrix, int k, int size, int &position)
+{
+    position = ((position / size + k) % size) * size + position % size; // położenie na osi y + przesunięcie na osi x
+    local_cell = rget(matrix + position).wait();
 }
 
-void cannon_algorithm(int* A, int* B, int* C, int size, int myid, int numprocs) {
-    MPI_Comm comm;
-    int dims[2] = { size, size };
-    int periods[2] = { 1, 1 };
-    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &comm);
-
-    int local_A = A[myid];
-    int local_B = B[myid];
+void cannon_algorithm(global_ptr<int> A, global_ptr<int> B, global_ptr<int> C, int size, int myid, int numprocs)
+{
+    int local_A = 0;
+    int local_B = 0;
     int local_C = 0;
+    int position_A = myid;
+    int position_B = myid;
 
     int first_shift_horizontal = myid / size;
     int first_shift_vertical = myid % size;
-    shift_matrix_horizontal(&local_A, first_shift_horizontal, comm);
-    shift_matrix_vertical(&local_B, first_shift_vertical, comm);
+    shift_matrix_horizontal(local_A, A, first_shift_horizontal, size, position_A);
+    shift_matrix_vertical(local_B, B, first_shift_vertical, size, position_B);
     local_C += local_A * local_B;
 
-    for (int step = 1; step < dims[0]; ++step) {
-        shift_matrix_horizontal(&local_A, 1, comm);
-        shift_matrix_vertical(&local_B, 1, comm);
+    for (int step = 1; step < size; ++step)
+    {
+        shift_matrix_horizontal(local_A, A, 1, size, position_A);
+        shift_matrix_vertical(local_B, B, 1, size, position_B);
         local_C += local_A * local_B;
     }
 
-    if (myid == 0) {
-        C[0] = local_C;
-        MPI_Status status;
-        for (int i = 1; i < numprocs; ++i) {
-            MPI_Recv(C + i, 1, MPI_INT, i, 100 + i, MPI_COMM_WORLD, &status);
-        }
-    }
-    else {
-        MPI_Send(&local_C, 1, MPI_INT, 0, 100 + myid, MPI_COMM_WORLD);
-    }
-
-    MPI_Comm_free(&comm);
+    upcxx::rput(local_C, C + myid).wait();
+    upcxx::barrier();
 }
 
-bool read_matrix(const char* file_name, int size, int* matrix)
+bool read_matrix(const char *file_name, int size, global_ptr<int> matrix)
 {
-    FILE* file = fopen(file_name, "r");
-    if (file == NULL)
+    ifstream file(file_name);
+    if (!file.is_open())
     {
-        printf("ERROR: File with data cannot be found\n");
+        cout << "ERROR: File with data cannot be found\n";
         return false;
     }
-    for (int i = 0; i < size * size; i++)
+    int value;
+    for (int i = 0; i < size; i++)
     {
-        int element;
-        fscanf(file, "%d", &element);
-        matrix[i] = element;
+        for (int j = 0; j < size; j++)
+        {
+            if (file >> value)
+            {
+                rput(value, matrix + i * size + j).wait();
+            }
+            else
+            {
+                cerr << "Error reading file at position " << i << "," << j << endl;
+                return -1;
+            }
+        }
     }
-    fclose(file);
+    file.close();
     return true;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        printf("Please enter three arguments <string1> <string2> <integer>");
+int main(int argc, char *argv[])
+{
+    upcxx::init();
+
+    int myid = upcxx::rank_me();
+    int numprocs = upcxx::rank_n();
+    broadcast(numprocs, 0).wait();
+
+    if (argc != 4)
+    {
+        if (myid == 0)
+        {
+            cout << "Please enter three arguments <string1> <string2> <integer>";
+        }
+        upcxx::finalize();
         return EXIT_FAILURE;
     }
 
-    //Init
-    int myid, numprocs;
-    int element_width = 2;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-    MPI_Bcast(&numprocs, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-    //Create matrix A and B
     int size = std::atoi(argv[3]);
-    if (size * size != numprocs) {
-        if (myid == 0) {
-            printf("This program requires one process for every matrix element!\n");
+    if (size * size != numprocs)
+    {
+        if (myid == 0)
+        {
+            cout << "This program requires one process for every matrix element!\n";
         }
-        MPI_Finalize();
+        upcxx::finalize();
         return EXIT_FAILURE;
     }
 
-    int* matrix_a = (int*)malloc(size * size * sizeof(int));
-    int* matrix_b = (int*)malloc(size * size * sizeof(int));
-    int* matrix_c = (int*)calloc(size * size, sizeof(int));
-
-    bool success_reading_data = false;
-    if (myid == 0 && read_matrix(argv[1], size, matrix_a) && read_matrix(argv[2], size, matrix_b)) {
-        success_reading_data = true;
-        printf("Matrix A\n");
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                printf("%*d ", element_width, matrix_a[i * size + j]);
-            }
-            printf("\n");
+    global_ptr<int> matrix_a = nullptr, matrix_b = nullptr, matrix_c = nullptr;
+    if (myid == 0)
+    {
+        matrix_c = new_array<int>(size * size);
+        for (int i = 0; i < numprocs; i++)
+        {
+            rput(0, matrix_c + i).wait();
         }
+    }
+    matrix_c = broadcast(matrix_c, 0).wait();
 
-        printf("Matrix B\n");
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                printf("%*d ", element_width, matrix_b[i * size + j]);
+    int success_reading_data = 0;
+    if (myid == 0)
+    {
+        matrix_a = new_array<int>(size * size);
+        matrix_b = new_array<int>(size * size);
+
+        if (read_matrix(argv[1], size, matrix_a) && read_matrix(argv[2], size, matrix_b))
+        {
+            success_reading_data = 1;
+            cout << "Matrix A\n";
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    int value = rget(matrix_a + i * size + j).wait();
+                    cout << setw(2) << value << " ";
+                }
+                cout << endl;
             }
-            printf("\n");
+
+            cout << "\nMatrix B\n";
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    int value = rget(matrix_b + i * size + j).wait();
+                    cout << setw(2) << value << " ";
+                }
+                cout << endl;
+            }
+            cout << endl;
         }
-        printf("\n");
     }
 
-    MPI_Bcast(&success_reading_data, 1, MPI_C_BOOL, 0, MPI_COMM_WORLD);
-    if (!success_reading_data) {
-        MPI_Finalize();
+    broadcast(&success_reading_data, 1, 0).wait();
+    upcxx::barrier();
+
+    if (success_reading_data != 1)
+    {
+        upcxx::finalize();
         return EXIT_FAILURE;
     }
 
-    MPI_Bcast(matrix_a, size * size, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(matrix_b, size * size, MPI_INT, 0, MPI_COMM_WORLD);
+    matrix_a = broadcast(matrix_a, 0).wait();
+    matrix_b = broadcast(matrix_b, 0).wait();
 
-    //Cannon
     cannon_algorithm(matrix_a, matrix_b, matrix_c, size, myid, numprocs);
 
-    if (myid == 0) {
-        printf("Result matrix:\n");
-        for (int i = 0; i < size; ++i) {
-            for (int j = 0; j < size; ++j) {
-                printf("%*d ", element_width, matrix_c[i * size + j]);
+    if (myid == 0)
+    {
+        cout << "Result matrix:\n";
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                int value = rget(matrix_c + i * size + j).wait();
+                cout << setw(2) << value << " ";
             }
-            printf("\n");
+            cout << endl;
         }
+
+        upcxx::delete_array(matrix_a);
+        upcxx::delete_array(matrix_b);
+        upcxx::delete_array(matrix_c);
     }
 
-    //Clear memory
-    free(matrix_a);
-    free(matrix_b);
-    free(matrix_c);
-    MPI_Finalize();
+    upcxx::finalize();
     return 0;
 }
